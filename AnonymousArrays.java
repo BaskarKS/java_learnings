70fa3caae419a78c6d0b54f6cc19b2cf028f0284
Baskar K S <baskarks@gmail.com>
2019-03-11 23:30:34 +0530

    modified in Branch, add more constants

--- AnonymousArrays.java
+++ AnonymousArrays.java
@@ -2,7 +2,7 @@ public class AnonymousArrays
 {
 		public static void main(String[] args)
 		{
-			int[] singleDimenArr = new int[] {10,20,30};
+			int[] singleDimenArr = new int[] {10,20,30,40,50};
 			int[][] doubleAnnonArr = new int[][]{{1,2,3}, {4,5}};
 			
 			AnonymousArrays.sum(new int[] {4,5,6});
